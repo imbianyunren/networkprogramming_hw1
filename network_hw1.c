e5f229205ff408059e00838e0277d47698f11b13
imbianyuanren <60705979+imbianyunren@users.noreply.github.com>
2021-11-16 21:51:41 +0800

    Update network_hw1.c

--- network_hw1.c
+++ network_hw1.c
@@ -27,45 +27,44 @@ void sigchld(int signo)
 {
     pid_t pid;
 
-    while ((pid = waitpid(-1, NULL, WNOHANG)) > 0)
-        ;
+    while ((pid = waitpid(-1, NULL, WNOHANG)) > 0);
 }
 void handler(int cli)
 {
     char buf[65536];
     int img, len, tmpcnt = 0;
-    FILE *f_inserv;
+    FILE *f_inserv; //在伺服器上建立的檔案
     char *ptr;
     char filename[128];
 
 
     memset(buf, 0, sizeof(buf));
-    read(cli, buf, sizeof(buf));
+    read(cli, buf, sizeof(buf)); //讀取socketfd內容
 
     if (strncmp(buf, "GET /shrimp.jpeg", 16) == 0)
-    {
+    {//讀取圖片時要做的開檔步驟
     	//printf("%s",buf);
         img = open("shrimp.jpeg", O_RDONLY);
         sendfile(cli, img, NULL, 5000000);
         close(img);
     }
     else if (strncmp(buf, "POST /", 6) == 0)
-    {
+    {//上傳檔案要做的內容
         ptr = strstr(buf, "filename");
         //printf("%s",buf);
         if (ptr)
         {
-            len = strlen("filename");
-            ptr += len + 2;
+            len = strlen("filename");//buffer有一段是filename="xxx.txt"(txt是舉例)
+            ptr += len + 2;//+2是因為="
             while (*ptr != '\"')
             {
-                filename[tmpcnt++] = *ptr++;
+                filename[tmpcnt++] = *ptr++;//讀到檔案名稱尾巴(")
             }
             filename[tmpcnt] = '\0';
 
             while(!(*(ptr-4)=='\r' && *(ptr-3)=='\n' && *(ptr-2)=='\r' && *(ptr-1)=='\n')) ptr++; //文件開頭
             char *tail=buf+strlen(buf)-3;
-				while(*tail!='\r') tail--; //文件結尾
+		while(*tail!='\r') tail--; //文件結尾
 
             f_inserv = fopen(filename, "w");
             while(ptr!=tail){
@@ -74,12 +73,12 @@ void handler(int cli)
             }
             fputc(*ptr,f_inserv);
             fclose(f_inserv);
-            write(cli, web, sizeof(web));
+            write(cli, web, sizeof(web)); 
         }
         else
             write(cli, web, sizeof(web));
     }
-    else{
+    else{//基本上就是處理GET / 的要求，然後將網頁內容輸出到客戶端
     	//printf("%s",buf);
         write(cli, web, sizeof(web));
         }
@@ -88,34 +87,39 @@ int main(void)
 {
     int serv, cli;
     struct sockaddr_in server_addr;
-
+    
+    //create socket
     serv = socket(AF_INET, SOCK_STREAM, 0);
-
+    
+    //連線設定
     bzero(&server_addr, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(8080);
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY); //使用任何在本機的對外ip
+    
+    //網路監視器
     bind(serv, (struct sockaddr *)&server_addr, sizeof(server_addr));
-
+    
+    //監聽
     int listenfd = listen(serv, 10);
 
     signal(SIGCHLD, sigchld);
 
     while (1)
     {
+        //waiting for connection
         cli = accept(serv, NULL, NULL);
-
+        //fork出子行程
         pid_t child = fork();
 
         if (child == 0)
-        {
+        {//child
             close(listenfd);
             handler(cli);
             exit(0);
         }
         else
-        {
+        {//parent
             close(cli);
         }
     }
